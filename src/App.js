0a4d553f32a1a01de99be8e06f7fcdfec2f352d1
Clancinio <dean.clancy91@gmail.com>
2021-07-26 16:08:48 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import "./calendar.css";
 
 function App() {
+  const handleDateSelect = (e) => {
+    console.log("Please enter a new title for your event");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cal">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          selectable={true}
+          dateClick={handleDateSelect}
+          weekends={false}
+          eventColor="#378006"
+          backgroundColor="#000565"
+          events={[
+            { title: "1 Available", date: "2021-07-01" },
+            { title: "2 Available", date: "2021-07-02" },
+            { title: "3 Avalable", date: "2021-06-25" },
+          ]}
+        />
+      </div>
+      <div className="side-bar">
+        <h2>Available Slots</h2>
+      </div>
     </div>
   );
 }
