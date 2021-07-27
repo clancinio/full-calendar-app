6ca174a33a6e299f775414e42b2c1ae685b6707a
Clancinio <dean.clancy91@gmail.com>
2021-07-27 15:20:26 +0100

    Add mock response

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import "./App.css";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction";
 import "./calendar.css";
 
 function App() {
-  const handleDateSelect = (e) => {
-    console.log("Please enter a new title for your event");
+  const handleDateSelect = (selectInfo) => {
+    //alert("You have clicked on a date");
+
+    let calendarApi = selectInfo.dateStr;
+
+    console.log(calendarApi);
+
+    //calendarApi.unselect() // clear date selection
   };
+
+  const handleDateClick = (info) => {
+    const view = info;
+
+    console.log(view);
+  };
+
+  // MOCK RESPOSNSE
+  const response = [
+    { id: 1, slot_id: 1, booked_by:"Jamie", date: "2021-07-28", display: "background"},
+    { id: 1, slot_id: 1, booked_by:"Jamie", date: "2021-07-29"  },
+    { id: 1, slot_id: 1, booked_by:"Jamie", date:  "2021-07-30" },
+    { id: 1, slot_id: 1, booked_by:"Jamie", date:  "2021-08-02" },
+    { id: 1, slot_id: 1, booked_by:"Jamie", date:  "2021-08-05" },
+    { id: 1, slot_id: 1, booked_by:"Jamie", date:  "2021-08-06" },
+    { id: 1, slot_id: 1, booked_by:"Jamie", date:  "2021-08-18" },
+  ];
+
+ // MAP THE RESPONSE TO EVENTS PROP
+  const events = response.map(x => (
+    {title: "1 Available", date: x.date})
+    )
 
   return (
     <div className="App">
       <div className="cal">
         <FullCalendar
-          plugins={[dayGridPlugin]}
+          plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           selectable={true}
-          dateClick={handleDateSelect}
+          select={handleDateSelect}
+          dateClick={handleDateClick}
           weekends={false}
           eventColor="#378006"
-          backgroundColor="#000565"
-          events={[
-            { title: "1 Available", date: "2021-07-01" },
-            { title: "2 Available", date: "2021-07-02" },
-            { title: "3 Avalable", date: "2021-06-25" },
-          ]}
+          events={events}
         />
       </div>
       <div className="side-bar">
